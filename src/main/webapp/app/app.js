237f335f3291eef03b9937f5ce73aaa00497fe52
clopez@apci.gob.pe <clopez@apci.gob.pe>
2015-10-14 19:47:42 -0500

    -

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -2,8 +2,8 @@ var app = angular.module('main',['ngRoute'])
 
 .constant("APP", 
 		{
-		    "URL"		: "/appbase/",
-		    "URL_API"	: "/appbase/api/v1/",    
+		    "URL"		: "/supervisores/",
+		    "URL_API"	: "/supervisores/api/v1/",    
 		}
 )
 
